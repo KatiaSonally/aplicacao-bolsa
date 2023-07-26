fb226e1323d34cfbce55a99bab05be1eece65bd4
KatiaSonally <katiasonally@yahoo.com.br>
2023-07-26 07:40:25 -0300

    fix: prevents access to the template

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -3,7 +3,7 @@ import { View } from "./view.js";
 
 export class NegociacoesView extends View<Negociacoes> {
 
-    template(model: Negociacoes): string {
+    protected template(model: Negociacoes): string {
         return `
         <table class="table table-rover table-bordered">
             <thead>
