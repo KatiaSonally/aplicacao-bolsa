65cd09f231c613c55d176e2cef17820ca8dccc40
KatiaSonally <katiasonally@yahoo.com.br>
2023-07-26 13:17:31 -0300

    refactor: it passes the trade creation method to the models folder

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -14,4 +14,12 @@ export class Negociacao {
         const data = new Date(this._data.getTime());
         return this._data;
     }
+
+    public criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp,','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(date, quantidade, valor); 
+    }
 }
