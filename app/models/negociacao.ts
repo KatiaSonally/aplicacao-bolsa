a40b25d553486409312b14558daa0c0a3ec43add
KatiaSonally <katiasonally@yahoo.com.br>
2023-07-27 15:16:19 -0300

    feat: assigns the classification static to the trade creation method

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -15,7 +15,7 @@ export class Negociacao {
         return this._data;
     }
 
-    public criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
+    public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
         const exp = /-/g;
         const date = new Date(dataString.replace(exp,','));
         const quantidade = parseInt(quantidadeString);
